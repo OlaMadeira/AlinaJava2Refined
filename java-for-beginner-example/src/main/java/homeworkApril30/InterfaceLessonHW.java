15ec526e690fab04c732e45faa54dd5c892d9d87
OlaMadeira <alinarossol"gmailcom>
2020-05-09 23:44:23 +0300

    готовый run

--- java-for-beginner-example/src/main/java/homeworkApril30/InterfaceLessonHW.java
+++ java-for-beginner-example/src/main/java/homeworkApril30/InterfaceLessonHW.java
@@ -3,28 +3,33 @@ package homeworkApril30;
 import object.Street;
 import object.transport.Car;
 import object.transport.Plane;
+import org.w3c.dom.ls.LSOutput;
 
 public class InterfaceLessonHW {
     public static void run() {
         int cash = 100000;
-        Repairable[] repairables = new Repairable[]{new Car(), new Street(), new Plane()};
+        Repairable[] repairables = new Repairable[]{new Plane(), new Car(), new Street(),};
         for (Repairable repairableObject : repairables) {
             if (repairableObject instanceof Plane && ((Plane) repairableObject).isNeedRepair) {
 
                 Plane currentPlane = (Plane) repairableObject;
                 currentPlane.repair(cash);
+                cash -= currentPlane.costOfRepair;
             }
 
-        if ( repairableObject instanceof Car && ((Car) repairableObject).isNeedRepair){
-            Car currentCar = (Car) repairableObject;
-            currentCar.repair(cash);
-        }
-            if ( repairableObject instanceof Street && repairableObject.isNeedToRepair()){
+            if (repairableObject instanceof Car && ((Car) repairableObject).isNeedRepair) {
+                Car currentCar = (Car) repairableObject;
+                currentCar.repair(cash);
+                cash -= 10;
+
+            }
+            if (repairableObject instanceof Street && repairableObject.isNeedToRepair()) {
                 Street currentStreet = (Street) repairableObject;
                 currentStreet.repair(cash);
             }
 
         }
+        System.out.println(cash);
 
     }
 }
